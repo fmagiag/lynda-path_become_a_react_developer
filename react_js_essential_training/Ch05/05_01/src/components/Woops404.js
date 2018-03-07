cccb13c70892c2ab8c61c20320629acb57684a96
Fernando Magiag <fernando@magiag.com>
2018-03-07 14:51:42 -0300

    DEV Incorporating the router

---
+++ react_js_essential_training/Ch05/05_01/src/components/Woops404.js
@@ -0,0 +1,4 @@
+export const Woops404 = () =>
+    <div>
+        <h1>Woops, route not found</h1>
+    </div>
--- react_js_essential_training/Ch05/05_01/src/index.js
+++ react_js_essential_training/Ch05/05_01/src/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 import { render } from 'react-dom'
 import './stylesheets/ui.scss'
 import { App } from './components/App'
+import { Woops404 } from './components/Woops404'
+import { Router, Route, hashHistory } from 'react-router'
 
 window.React = React
 
 render(
-	<App />,
+	<Router history={hashHistory}>
+		<Route path="/" component={App}/>
+		<Route path="*" component={Woops404}/>
+	</Router>,
 	document.getElementById('react-container')
 )
