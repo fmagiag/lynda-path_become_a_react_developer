695ad60ab750560dfdb7f2a85af74c50717d6f66
Fernando Magiag <fernando@magiag.com>
2018-03-07 20:00:52 -0300

    DEV Navigating with the link component

--- react_js_essential_training/Ch05/05_01/src/components/App.js
+++ react_js_essential_training/Ch05/05_01/src/components/App.js
@@ -2,6 +2,7 @@ import { createClass } from 'react'
 import { SkiDayList } from './SkiDayList'
 import { SkiDayCount } from './SkiDayCount'
 import { AddDayForm } from './AddDayForm';
+import { Menu } from './Menu';
 
 export const App = createClass({
 	getInitialState() {
@@ -36,21 +37,20 @@ export const App = createClass({
 	render() {
 		return (
 			<div className="app">
+			<Menu />
 			{(this.props.location.pathname === "/") ? 
 			<SkiDayCount total={this.countDays()}
-			powder={this.countDays(
-					"powder"
-				)}
-			backcountry={this.countDays(
-					"backcountry"
-				)}/> :
+						 powder={this.countDays(
+							"powder"
+						 )}
+						 backcountry={this.countDays(
+							"backcountry"
+				  	     )}/> :
 			(this.props.location.pathname === "/add-day") ?
 
 			<AddDayForm /> :
 			<SkiDayList days={this.state.allSkiDays}/>
 			}
-				
-				
 			</div>
 		)
 	}
