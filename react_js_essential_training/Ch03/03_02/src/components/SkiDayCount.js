b6680c829f6c681b310d87cd078bf80e1f514923
Fernando Magiag <fernando@magiag.com>
2018-03-06 21:21:35 -0300

    DEV Adding react-icons FINISH React Components

--- react_js_essential_training/Ch03/03_02/src/components/SkiDayCount.js
+++ react_js_essential_training/Ch03/03_02/src/components/SkiDayCount.js
@@ -1,8 +1,12 @@
 import '../stylesheets/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 
 const percentToDecimal = (decimal) => {
     return ((decimal * 100) + '%')
 }
+
 const calcGoalsProgress = (total, goal) => {
     return percentToDecimal(total/goal)
 }
@@ -11,14 +15,17 @@ export const SkiDayCount = ({total, powder, backcountry, goal}) => (
             <div className="ski-day-count">
                 <div className="total-days">
                     <span>{total}</span>
+                    <Calendar />
                     <span>days</span>
                 </div>
                 <div className="powder-days">
                     <span>{powder}</span>
+                    <SnowFlake />
                     <span>days</span>
                 </div>
                 <div className="backcountry-days">
                     <span>{backcountry}</span>
+                    <Terrain />
                     <span>days</span>
                 </div>
                 <div className="backcountry-days">
