d4b947702e23b9b8935a899fde878c9cba8a411c
Fernando Magiag <fernando@magiag.com>
2018-03-20 22:40:03 -0300

    DEV Using refs in stateless components

--- react_js_essential_training/Ch06/06_01/src/components/AddDayForm.js
+++ react_js_essential_training/Ch06/06_01/src/components/AddDayForm.js
@@ -1,44 +1,40 @@
 import { PropTypes, Component } from 'react'
 
-export class AddDayForm extends Component {
-
-	constructor(props) {
-		super(props)
-		this.submit = this.submit.bind(this)
-	}
-
-	submit(e) {
+export const AddDayForm = ({ resort, 
+							 date, 
+							 powder, 
+							 backcountry}) => {
+	let _resort, _date, _powder, _backcountry
+	const submit = (e) => {
 		e.preventDefault()
-		console.log('resort', this.refs.resort.value)
-		console.log('date', this.refs.date.value)
-		console.log('powder', this.refs.powder.checked)
-		console.log('backcountry', this.refs.backcountry.checked)
+		console.log('resort', _resort.value)
+		console.log('date', _date.value)
+		console.log('powder', _powder.checked)
+		console.log('backcountry', _backcountry.checked)
 	}
-	render(){
 
-		const { resort, date, powder, backcountry} = this.props
 		return (
-			<form onSubmit={this.submit} className="add-day-form">
+			<form onSubmit={submit} className="add-day-form">
 
 				<label htmlFor="resort">Resort Name</label>
 				<input id="resort"
 					   type="text"
 					   required
 					   defaultValue={resort}
-					   ref="resort" />
+					   ref={input => _resort = input} />
 
 				<label htmlFor="date">Date</label>
 				<input id="date" 
 					   type="date" 
 					   required 
 					   defaultValue={date}
-					   ref="date"/>
+					   ref={input => _date = input}/>
 
 				<div>
 					<input id="powder" 
 						   type="checkbox"
 						   defaultChecked={powder}
-						   ref="powder" />
+						   ref={input => _powder = input}/>
 					<label htmlFor="resort">Powder Day</label>
 				</div>
 
@@ -46,7 +42,7 @@ export class AddDayForm extends Component {
 					<input id="backcountry" 
 						   type="checkbox"
 						   defaultChecked={backcountry}
-						   ref="backcountry"/>
+						   ref={input => _backcountry = input}/>
 					<label htmlFor="backcountry">
 						Backcountry Day
 					</label>
@@ -54,7 +50,6 @@ export class AddDayForm extends Component {
 				<button>Add Day</button>
 			</form>
 		)
-	}
 }
 
 AddDayForm.defaultProps = {
